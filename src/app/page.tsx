e0343f8eab6c4375617e9f8cc710ef4fa25c074b
Josh <joshua.ghattas@gmail.com>
2025-09-02 18:22:12 -0500

    Added a marquee to the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,16 @@ import {
 import Image from "next/image";
 import GlowBg from "@/components/ui/backgrounds/glow-bg";
 import Card from "@/components/ui/global/card";
+import { GiPokerHand, GiCardJackClubs, GiCoinflip, GiLandMine } from "react-icons/gi";
+import { RxColorWheel } from "react-icons/rx";
+import Marquee from "react-fast-marquee";
+import { PiJoystickLight } from "react-icons/pi";
 
 export default function Home() {
   return (
     <main className="relative">
       <div className="relative z-10">
-        <div className="relative w-full h-screen overflow-hidden bg-background-900">
+        <div className="relative w-full h-screen pb-2 overflow-hidden bg-background-900">
           <GlowBg />
           <header className="relative w-full h-[50px] flex items-center bg-background-800 px-2 justify-between py-2 border-b border-b-background-700 z-20">
             <div className="flex items-center gap-2">
@@ -45,12 +49,47 @@ export default function Home() {
             </div>
           </header>
           <div className="relative z-30 flex flex-col items-center justify-center w-full h-full mb-20">
-            <h1 className="mb-2 font-bold text-7xl">ChipIn</h1>
-            <h1 className="text-4xl font-bold">Gambling without the risk</h1>
-            <div className="flex justify-center gap-3 mt-20">
-              <Card>Poker</Card>
-              <Card>Blackjack</Card>
-            </div>
+            <h1 className="mb-8 font-bold text-7xl">ChipIn</h1>
+            <h1 className="px-4 text-4xl font-bold">Gambling without the risk</h1>
+            <Marquee autoFill pauseOnHover gradient gradientColor="rgb(24, 25, 27)" className="flex justify-center mt-20">
+              <Card color="red" className="mx-2">
+                <h1 className="mb-2 text-3xl text-red-400">Poker</h1>
+                <div className="flex justify-center">
+                  <GiPokerHand size="70" className="text-red-400" />
+                </div>
+              </Card>
+              <Card color="orange" className="mx-2">
+                <h1 className="mb-2 text-3xl text-orange-400">Blackjack</h1>
+                <div className="flex justify-center">
+                  <GiCardJackClubs size="70" className="text-orange-400" />
+                </div>
+              </Card>
+              <Card color="yellow" className="mx-2">
+                <h1 className="mb-2 text-3xl text-yellow-400">Coinflip</h1>
+                <div className="flex justify-center">
+                  <GiCoinflip size="70" className="text-yellow-400" />
+                </div>
+              </Card>
+              <Card className="mx-2">
+                <h1 className="mb-2 text-3xl text-primary-400">Roulette</h1>
+                <div className="flex justify-center">
+                  <RxColorWheel size="70" className="text-primary-400" />
+                </div>
+              </Card>
+              <Card color="blue" className="mx-2">
+                <h1 className="mb-2 text-3xl text-blue-400">Mines</h1>
+                <div className="flex justify-center">
+                  <GiLandMine size="70" className="text-blue-400" />
+                </div>
+              </Card>
+              <Card color="purple" className="mx-2">
+                <h1 className="mb-2 text-3xl text-purple-400">Slots</h1>
+                <div className="flex justify-center">
+                  <PiJoystickLight size="70" className="text-purple-400" />
+                </div>
+              </Card>
+            </Marquee>
+            <h1 className="px-4 mt-10 text-xl font-bold">All of these games and more, without ever having to spend a penny!</h1>
           </div>
         </div>
 
--- src/components/ui/global/card.tsx
+++ src/components/ui/global/card.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import clsx from "clsx";
 
 type CardProps = React.HTMLAttributes<HTMLDivElement>;
 
-export default function Card({ children, className = "", ...rest }: CardProps) {
+export default function Card({
+  children,
+  className = "",
+  color = "green",
+  ...rest
+}: CardProps) {
   return (
     <div
-      className={`p-[20px]! rounded-[12px] border-2 border-primary-400 backdrop-blur-md backdrop-filter backdrop-opacity-50 bg-primary-900 ${className}`}
+      className={clsx(
+        `p-[20px]! rounded-[12px] border-2 backdrop-blur-md backdrop-filter backdrop-opacity-50 ${className}`,
+        color == "green"
+          ? "border-primary-400 bg-primary-900"
+          : color == "blue"
+          ? "border-accent-400 bg-accent-900"
+          : color == "red"
+          ? "border-red-500 bg-red-900"
+          : color == "purple"
+          ? "border-purple-400 bg-purple-900"
+          : color == "orange"
+          ? "border-orange-400 bg-orange-900"
+          : color == "yellow"
+          ? "border-yellow-400 bg-yellow-900"
+          : "border-primary-400 bg-primary-900"
+      )}
       {...rest}
     >
       {children}
