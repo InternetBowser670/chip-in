06a9c51f81e198372aa833c4236cbc1764197725
Josh <joshua.ghattas@gmail.com>
2025-09-03 19:07:59 -0500

    Fixed the marquee being empty at times

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,12 @@ import {
 import Image from "next/image";
 import GlowBg from "@/components/ui/backgrounds/glow-bg";
 import Card from "@/components/ui/global/card";
-import { GiPokerHand, GiCardJackClubs, GiCoinflip, GiLandMine } from "react-icons/gi";
+import {
+  GiPokerHand,
+  GiCardJackClubs,
+  GiCoinflip,
+  GiLandMine,
+} from "react-icons/gi";
 import { RxColorWheel } from "react-icons/rx";
 import Marquee from "react-fast-marquee";
 import { PiJoystickLight } from "react-icons/pi";
@@ -51,8 +56,52 @@ export default function Home() {
           </header>
           <div className="relative z-30 flex flex-col items-center justify-center w-full h-full mb-20">
             <h1 className="mb-8 font-bold text-7xl">ChipIn</h1>
-            <h1 className="px-4 text-4xl font-bold">Gambling without the risk</h1>
-            <Marquee autoFill pauseOnHover gradient gradientColor="rgb(24, 25, 27)" className="flex justify-center mt-20">
+            <h1 className="px-4 text-4xl font-bold">
+              Gambling without the risk
+            </h1>
+            <Marquee
+              autoFill
+              pauseOnHover
+              gradient
+              gradientColor="rgb(24, 25, 27)"
+              className="flex justify-center mt-20"
+            >
+              <Card color="red" className="mx-2">
+                <h1 className="mb-2 text-3xl text-red-400">Poker</h1>
+                <div className="flex justify-center">
+                  <GiPokerHand size="70" className="text-red-400" />
+                </div>
+              </Card>
+              <Card color="orange" className="mx-2">
+                <h1 className="mb-2 text-3xl text-orange-400">Blackjack</h1>
+                <div className="flex justify-center">
+                  <GiCardJackClubs size="70" className="text-orange-400" />
+                </div>
+              </Card>
+              <Card color="yellow" className="mx-2">
+                <h1 className="mb-2 text-3xl text-yellow-400">Coinflip</h1>
+                <div className="flex justify-center">
+                  <GiCoinflip size="70" className="text-yellow-400" />
+                </div>
+              </Card>
+              <Card className="mx-2">
+                <h1 className="mb-2 text-3xl text-primary-400">Roulette</h1>
+                <div className="flex justify-center">
+                  <RxColorWheel size="70" className="text-primary-400" />
+                </div>
+              </Card>
+              <Card color="blue" className="mx-2">
+                <h1 className="mb-2 text-3xl text-blue-400">Mines</h1>
+                <div className="flex justify-center">
+                  <GiLandMine size="70" className="text-blue-400" />
+                </div>
+              </Card>
+              <Card color="purple" className="mx-2">
+                <h1 className="mb-2 text-3xl text-purple-400">Slots</h1>
+                <div className="flex justify-center">
+                  <PiJoystickLight size="70" className="text-purple-400" />
+                </div>
+              </Card>
               <Card color="red" className="mx-2">
                 <h1 className="mb-2 text-3xl text-red-400">Poker</h1>
                 <div className="flex justify-center">
@@ -90,7 +139,9 @@ export default function Home() {
                 </div>
               </Card>
             </Marquee>
-            <h1 className="px-4 mt-10 text-xl font-bold">All of these games and more, without ever having to spend a penny!</h1>
+            <h1 className="px-4 mt-10 text-xl font-bold">
+              All of these games and more, without ever having to spend a penny!
+            </h1>
           </div>
           <ScrollDown />
         </div>
