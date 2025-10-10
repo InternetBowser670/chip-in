7c1d1a92f2f510ff38f92fed76b125d9d1c4d98c
Josh <joshua.ghattas@gmail.com>
2025-10-10 10:41:41 -0500

    Added auth verification

--- src/app/(with-sidebar)/layout.tsx
+++ src/app/(with-sidebar)/layout.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import ClaimChips from "@/components/ui/global/claim-chips";
 import ChipCount from "@/components/ui/global/chip-count";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function SidebarLayout({
   children,
@@ -10,24 +11,31 @@ export default function SidebarLayout({
 }>) {
   return (
     <>
-      <div className="flex h-screen!">
-        <div className="w-50 h-full! bg-background-700">
-          <div className="flex flex-col items-center mt-2">
-            <Link href={'/'} className="flex justify-center">
-              <Image
-                src={"/chip-in-logo.png"}
-                width={50}
-                height={50}
-                alt="Chip In logo"
-              />
-              <p className="flex items-center text-lg!">ChipIn</p>
-            </Link>
-            <ClaimChips />
-            <ChipCount />
+      <SignedIn>
+        <div className="flex h-screen!">
+          <div className="w-50 h-full! bg-background-700">
+            <div className="flex flex-col items-center mt-2">
+              <Link href={"/"} className="flex justify-center">
+                <Image
+                  src={"/chip-in-logo.png"}
+                  width={50}
+                  height={50}
+                  alt="Chip In logo"
+                />
+                <p className="flex items-center text-lg!">ChipIn</p>
+              </Link>
+              <ClaimChips />
+              <ChipCount />
+            </div>
           </div>
+          <div className="w-full h-full rounded-2xl">{children}</div>
         </div>
-        <div className="w-full h-full rounded-2xl">{children}</div>
-      </div>
+      </SignedIn>
+      <SignedOut>
+        <div className="flex items-center justify-center w-full h-screen">
+          Please sign in to access the dashboard.
+        </div>
+      </SignedOut>
     </>
   );
 }
