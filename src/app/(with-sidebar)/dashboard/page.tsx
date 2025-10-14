68e071c367b8d7ee552e813ec8063fa929929628
Josh <joshua.ghattas@gmail.com>
2025-10-13 21:31:43 -0500

    Added a stand-in page for the coinflip game

--- src/app/(with-sidebar)/dashboard/page.tsx
+++ src/app/(with-sidebar)/dashboard/page.tsx
@@ -1,3 +1,39 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Card from "@/components/ui/global/card";
+import { GiCoinflip } from "react-icons/gi";
+
 export default function Page() {
-    return <><div className="flex items-center justify-center w-full h-full">We&apos;re working on it...</div></>
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="w-full bg-gray-700 p-2">
+          <h2 className="font-bold pb-2 text-xl">Games:</h2>
+          <div className="flex gap-2">
+            <Card
+              chin
+              color="red"
+              className="mx-2 w-fit cursor-pointer"
+              onClick={() => router.push("/play/coinflip")}
+            >
+              <h1 className="mb-2 text-3xl text-red-400">Coinflip</h1>
+              <div className="flex justify-center">
+                <GiCoinflip size="70" className="text-red-400" />
+              </div>
+            </Card>
+            <Card
+              chin
+              color="blue"
+              className="mx-2 w-fit flex items-center justify-center"
+            >
+              <h1 className="mb-2 text-3xl text-blue-400">More coming soon!</h1>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/app/(with-sidebar)/play/coinflip/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <div className="flex justify-center items-center w-full h-full">
+        <h1 className="text-3xl font-bold">Coming soon!</h1>
+      </div>
+    </>
+  );
+}
