fabde1cc433c3e2953b8a6bebddb64bf80b40487
Josh <joshua.ghattas@gmail.com>
2025-10-15 18:36:00 -0500

    Added a coinflip animation

--- src/app/(with-sidebar)/play/coinflip/page.tsx
+++ src/app/(with-sidebar)/play/coinflip/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
 export default function Page() {
   return (
     <>
       <div className="flex justify-center items-center w-full h-full">
-        <h1 className="text-3xl font-bold">Coming soon!</h1>
+        <div className="flex-col items-center p-2 bg-gray-700 rounded-2xl text-center">
+          <h1 className="text-3xl font-bold">Coming soon!</h1>
+          <DotLottieReact
+            segment={[0, 44]}
+            src="https://lottie.host/097a8e78-8f21-46ab-8342-52feeab0a830/QVF4Cv44bH.lottie"
+            loop
+            autoplay
+          />
+          <p>Enjoy my coinflip animation for now</p>
+        </div>
       </div>
     </>
   );
