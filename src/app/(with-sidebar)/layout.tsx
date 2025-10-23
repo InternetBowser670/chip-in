de2e3c86900a79264d8d25781b8efe1c220134e1
Josh <joshua.ghattas@gmail.com>
2025-10-23 17:21:18 -0500

    Fixed CSS

--- src/app/(with-sidebar)/layout.tsx
+++ src/app/(with-sidebar)/layout.tsx
@@ -42,9 +42,7 @@ export default async function SidebarLayout({
                 <div className="flex justify-end gap-4 overflow-hidden">
                   <UserButton />
                   <div className="overflow-hidden">
-                    <p className="inline overflow-hidden">
-                      {user?.username}
-                    </p>
+                    <p className="inline overflow-hidden">{user?.username}</p>
                   </div>
                 </div>
               </div>
@@ -56,7 +54,7 @@ export default async function SidebarLayout({
       <SignedOut>
         <div className="flex h-screen!">
           <div className="w-50 h-full! bg-background-700">
-            <div className="flex flex-col items-center justify-between mt-2">
+            <div className="flex flex-col items-center justify-between h-full mt-2">
               <Link href={"/"} className="flex justify-center">
                 <Image
                   src={"/chip-in-logo.png"}
@@ -66,16 +64,17 @@ export default async function SidebarLayout({
                 />
                 <p className="flex items-center text-lg!">ChipIn</p>
               </Link>
-              <ClerkLoading>Loading user...</ClerkLoading>
-              <SignInButton />
-              <SignUpButton>
-                <button
-                  type="button"
-                  className="px-2 py-1 m-2 text-sm font-medium transition-all border-2 border-b-4 cursor-pointer rounded-2xl bg-accent-900 sm:text-base sm:px-5 border-accent-400 hover:bg-accent-800"
-                >
-                  Sign Up
-                </button>
-              </SignUpButton>
+              <div className="flex flex-col items-center mb-4">
+                <SignInButton />
+                <SignUpButton>
+                  <button
+                    type="button"
+                    className="px-2 py-1 m-2 text-sm font-medium transition-all border-2 border-b-4 cursor-pointer rounded-2xl bg-accent-900 sm:text-base sm:px-5 border-accent-400 hover:bg-accent-800"
+                  >
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </div>
             </div>
           </div>
           <div className="flex items-center justify-center w-full h-screen">
