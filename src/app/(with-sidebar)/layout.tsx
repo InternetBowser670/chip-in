ce6de455595a7ed9dbf4650abc914337c82e4ffa
Josh <joshua.ghattas@gmail.com>
2025-10-23 17:20:01 -0500

    Added user options to the bottom of the sidebar

--- src/app/(with-sidebar)/layout.tsx
+++ src/app/(with-sidebar)/layout.tsx
@@ -2,30 +2,52 @@ import Image from "next/image";
 import Link from "next/link";
 import ClaimChips from "@/components/ui/global/claim-chips";
 import ChipCount from "@/components/ui/global/chip-count";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
+import {
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function SidebarLayout({
+export default async function SidebarLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await currentUser();
+
   return (
     <>
       <SignedIn>
         <div className="flex h-screen!">
           <div className="w-50 h-full! bg-background-700">
-            <div className="flex flex-col items-center mt-2">
-              <Link href={"/"} className="flex justify-center">
-                <Image
-                  src={"/chip-in-logo.png"}
-                  width={50}
-                  height={50}
-                  alt="Chip In logo"
-                />
-                <p className="flex items-center text-lg!">ChipIn</p>
-              </Link>
-              <ClaimChips />
-              <ChipCount />
+            <div className="flex flex-col items-center justify-between h-full">
+              <div className="flex flex-col items-center mt-2">
+                <Link href={"/"} className="flex justify-center">
+                  <Image
+                    src={"/chip-in-logo.png"}
+                    width={50}
+                    height={50}
+                    alt="Chip In logo"
+                  />
+                  <p className="flex items-center text-lg!">ChipIn</p>
+                </Link>
+                <ClaimChips />
+                <ChipCount />
+              </div>
+              <div className="flex flex-col items-center mb-4 overflow-x-hidden">
+                <div className="flex justify-end gap-4 overflow-hidden">
+                  <UserButton />
+                  <div className="overflow-hidden">
+                    <p className="inline overflow-hidden">
+                      {user?.username}
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="w-full h-full rounded-2xl">{children}</div>
@@ -34,7 +56,7 @@ export default function SidebarLayout({
       <SignedOut>
         <div className="flex h-screen!">
           <div className="w-50 h-full! bg-background-700">
-            <div className="flex flex-col items-center mt-2">
+            <div className="flex flex-col items-center justify-between mt-2">
               <Link href={"/"} className="flex justify-center">
                 <Image
                   src={"/chip-in-logo.png"}
@@ -44,6 +66,16 @@ export default function SidebarLayout({
                 />
                 <p className="flex items-center text-lg!">ChipIn</p>
               </Link>
+              <ClerkLoading>Loading user...</ClerkLoading>
+              <SignInButton />
+              <SignUpButton>
+                <button
+                  type="button"
+                  className="px-2 py-1 m-2 text-sm font-medium transition-all border-2 border-b-4 cursor-pointer rounded-2xl bg-accent-900 sm:text-base sm:px-5 border-accent-400 hover:bg-accent-800"
+                >
+                  Sign Up
+                </button>
+              </SignUpButton>
             </div>
           </div>
           <div className="flex items-center justify-center w-full h-screen">
