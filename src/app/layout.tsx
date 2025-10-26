70b6132a6ff588b5a45b715e4abbf64b5a5dae78
Josh <joshua.ghattas@gmail.com>
2025-10-25 19:47:01 -0500

    Added Google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Providers } from "@/components/providers";
 import { Analytics } from "@vercel/analytics/next";
 import TimezoneSetter from "@/components/ui/timezone-setter";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Chip In",
@@ -23,6 +24,22 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html className="h-screen!" lang="en">
+        <head>
+          {/* Google Analytics */}
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-3GKZ3LXBM9"
+          />
+          <Script id="google-analytics-datalayer" strategy="afterInteractive">
+            {/* this just makes sure that Next doesn't check the code (trust me, it works) */}
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3GKZ3LXBM9');
+          `}
+          </Script>
+        </head>
         <body
           className={`${openSans.className} antialiased! h-full! min-h-screen! p-0 text-text-50! bg-background-900!`}
         >
