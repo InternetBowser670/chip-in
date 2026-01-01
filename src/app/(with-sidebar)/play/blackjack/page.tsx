478b295686611c481d5d1d5654d66b109d1d7e64
Josh <joshua.ghattas@gmail.com>
2026-01-01 07:21:11 -0600

    Added coming soon text to blackjack page

--- src/app/(with-sidebar)/play/blackjack/page.tsx
+++ src/app/(with-sidebar)/play/blackjack/page.tsx
@@ -1,7 +1,16 @@
 export default function BlackjackPage() {
   return (
     <div className="flex items-center justify-center h-full">
-      <h1 className="text-3xl font-bold">Coming soon!</h1>
+      <div className="h-[80%] w-[90%] bg-gray-700 rounded-2xl text-center overflow-auto flex">
+        <div className="w-auto h-full p-4 rounded-r-2xl bg-background-700">
+          <div className="flex flex-col items-center">
+            <h1 className="text-5xl font-bold">Blackjack</h1>
+          </div>
+        </div>
+        <div className="flex items-center justify-center flex-1 h-full">
+            <h1 className="text-3xl font-bold text-center">Coming Soon!</h1>
+        </div>
+      </div>
     </div>
   );
 }
