1ff4b9e5b70bdacedf334ca36ad25d0d3e78c61e
Josh <joshua.ghattas@gmail.com>
2026-01-27 17:55:21 -0600

    Added an animation to the chip counter

--- src/components/ui/global/chip-count.tsx
+++ src/components/ui/global/chip-count.tsx
@@ -2,6 +2,7 @@
 
 import { PiPokerChip } from "react-icons/pi";
 import { useChips } from "@/components/providers";
+import Counter from "@/components/Counter";
 
 export default function ChipCount() {
   const { chips } = useChips();
@@ -9,7 +10,13 @@ export default function ChipCount() {
   return (
     <div className="flex items-center gap-2 mt-2 text-xl font-bold">
       <PiPokerChip size={24} />
-      <span>{chips.toFixed(2).toLocaleString() }</span>
+      <Counter
+        gradientFrom="#484b51"
+        fontSize={17}
+        gradientHeight={2}
+        gap={1}
+        value={+chips.toFixed(2)}
+      />
     </div>
   );
 }
