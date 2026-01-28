95ccf3d80b98b76610dd7014517f543ab43b67d4
Josh <joshua.ghattas@gmail.com>
2026-01-28 05:44:15 -0600

    Redo hero section, add theming, etc.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import Image from "next/image";
-import Card from "@/components/ui/global/card";
+import { Card, CardFooter, CardTitle } from "@/components/ui/card";
 import {
   GiPokerHand,
   GiCardJackSpades,
@@ -21,8 +21,11 @@ import { RxColorWheel } from "react-icons/rx";
 import { PiJoystickLight } from "react-icons/pi";
 import ScrollDown from "@/components/ui/global/scroll-indicator";
 import { useRouter } from "next/navigation";
-import { PrimaryButtonChildren } from "@/components/ui/global/buttons";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import ModeToggle from "@/components/ui/theme-switcher";
+import ColorBends from "@/components/ColorBends";
+import GlassSurface from "@/components/GlassSurface";
 
 export default function Home() {
   const router = useRouter();
@@ -30,97 +33,163 @@ export default function Home() {
   return (
     <main className="relative w-screen h-screen overflow-x-hidden">
       <div className="relative z-10">
-        <div className="relative w-full h-screen pb-2 overflow-hidden bg-background-900">
-          <header className="fixed w-full h-12.5 flex items-center bg-background-800 px-2 justify-between py-2 border-b border-b-background-700 z-200">
-            <div className="flex items-center gap-2">
-              <Image
-                src={"/chip-in-logo.png"}
-                width={30}
-                height={30}
-                alt="Chip In logo"
-              />
-              <p className="text-lg font-bold">ChipIn</p>
-            </div>
-            <div className="flex items-center gap-4 py-2">
-              <ClerkLoading>Loading user...</ClerkLoading>
-              <SignedOut>
-                <SignInButton />
-                <SignUpButton>
-                  <button
-                    type="button"
-                    className="px-2 py-1 m-2 text-sm font-medium transition-all border-2 border-b-4 cursor-pointer rounded-2xl bg-accent-900 sm:text-base sm:px-5 border-accent-400 hover:bg-accent-800"
-                  >
-                    Sign Up
-                  </button>
-                </SignUpButton>
-              </SignedOut>
-              <SignedIn>
-                <PrimaryButtonChildren
-                  onClick={() => router.push("/dashboard")}
-                >
-                  Dashboard
-                </PrimaryButtonChildren>
-                <UserButton />
-              </SignedIn>
-            </div>
+        <div className="relative w-full h-screen overflow-hidden">
+          <header className="fixed flex justify-center w-full top-2 z-100 h-fit!">
+            <GlassSurface
+              width={"90%"}
+              backgroundOpacity={0.1}
+              className="fixed z-50 flex items-center justify-between w-full px-2 py-2 border-b h-fit border-b-background-700"
+              height={"fit"}
+            >
+              <div className="flex items-center justify-between w-full h-fit">
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={"/chip-in-logo.png"}
+                    width={30}
+                    height={30}
+                    alt="Chip In logo"
+                  />
+                  <p className="text-lg font-bold">ChipIn</p>
+                </div>
+                <div className="flex items-center gap-4">
+                  <ModeToggle />
+                  <ClerkLoading>Loading user...</ClerkLoading>
+                  <SignedOut>
+                    <SignInButton />
+                    <SignUpButton>
+                      <button
+                        type="button"
+                        className="px-2 py-1 m-2 text-sm font-medium transition-all border-2 border-b-4 cursor-pointer rounded-2xl bg-accent-900 sm:text-base sm:px-5 border-accent-400 hover:bg-accent-800"
+                      >
+                        Sign Up
+                      </button>
+                    </SignUpButton>
+                  </SignedOut>
+                  <SignedIn>
+                    <Button
+                      variant={"default"}
+                      onClick={() => router.push("/dashboard")}
+                    >
+                      Dashboard
+                    </Button>
+                    <UserButton />
+                  </SignedIn>
+                </div>
+              </div>
+            </GlassSurface>
           </header>
-          <div className="relative z-30 flex flex-col items-center justify-center w-full h-full mb-20">
+          <ColorBends className="absolute -z-1" />
+          <div className="absolute top-0 z-30 flex flex-col items-center justify-center w-full h-full mb-20">
             <h1 className="mb-8 font-bold text-7xl">ChipIn</h1>
             <h1 className="px-4 text-4xl font-bold">
               Gambling without the risk
             </h1>
             <div className="flex items-center justify-center mt-20">
-              <Card chin color="grayscale" className="mx-2 grayscale">
-                <h1 className="mb-2 text-3xl text-red-400">Poker</h1>
+              <Card className="p-4 mx-2">
                 <div className="flex justify-center">
-                  <GiPokerHand size="70" className="text-red-400" />
+                  <GiPokerHand size="70" />
                 </div>
+                <CardTitle className="text-4xl font-extrabold tracking-tight text-center scroll-m-20 text-balance">
+                  Poker
+                </CardTitle>
+                <CardFooter>
+                  <Button className="w-full" disabled>
+                    Coming Soon!
+                  </Button>
+                </CardFooter>
               </Card>
-              <Link href="/play/blackjack">
-                <Card chin color="orange" className="mx-2">
-                  <h1 className="mb-2 text-3xl text-orange-400">Blackjack</h1>
-                  <div className="flex justify-center">
-                    <GiCardJackSpades size="70" className="text-orange-400" />
-                  </div>
-                </Card>
-              </Link>
 
-              <Link href="/play/coinflip">
-                <Card chin color="yellow" className="mx-2">
-                  <h1 className="mb-2 text-3xl text-yellow-400">Coinflip</h1>
-                  <div className="flex justify-center">
-                    <GiCoinflip size="70" className="text-yellow-400" />
-                  </div>
-                </Card>
-              </Link>
-              <Card chin color="grayscale" className="mx-2 grayscale">
-                <h1 className="mb-2 text-3xl text-primary-400">Roulette</h1>
+              <Card className="p-4 mx-2">
                 <div className="flex justify-center">
-                  <RxColorWheel size="70" className="text-primary-400" />
+                  <GiCardJackSpades size="70" />
                 </div>
+                <CardTitle className="text-4xl font-extrabold tracking-tight text-center scroll-m-20 text-balance">
+                  Blackjack
+                </CardTitle>
+                <CardFooter>
+                  <Button
+                    onClick={() => router.push("/play/blackjack")}
+                    className="w-full"
+                  >
+                    Play now!
+                  </Button>
+                </CardFooter>
               </Card>
-              <Link href="/play/mines">
-                <Card chin color="blue" className="mx-2">
-                  <h1 className="mb-2 text-3xl text-blue-400">Mines</h1>
-                  <div className="flex justify-center">
-                    <GiUnlitBomb size="70" className="text-blue-400" />
-                  </div>
-                </Card>
-              </Link>
-              <Card chin color="grayscale" className="mx-2 grayscale">
-                <h1 className="mb-2 text-3xl text-purple-400">Slots</h1>
+
+              <Card className="p-4 mx-2">
                 <div className="flex justify-center">
-                  <PiJoystickLight size="70" className="text-purple-400" />
+                  <GiCoinflip size="70" />
                 </div>
+                <CardTitle className="text-4xl font-extrabold tracking-tight text-center scroll-m-20 text-balance">
+                  Coinflip
+                </CardTitle>
+                <CardFooter>
+                  <Button
+                    onClick={() => router.push("/play/coinflip")}
+                    className="w-full"
+                  >
+                    Play now!
+                  </Button>
+                </CardFooter>
               </Card>
-              <Card chin color="grayscale" className="mx-2 grayscale">
-                <h1 className="mb-2 text-3xl text-red-400">And More!</h1>
+
+              <Card className="p-4 mx-2">
                 <div className="flex justify-center">
-                  <GiPerspectiveDiceSixFacesSix
-                    size="70"
-                    className="text-red-400"
-                  />
+                  <RxColorWheel size="70" />
                 </div>
+                <CardTitle className="text-4xl font-extrabold tracking-tight text-center scroll-m-20 text-balance">
+                  Roulette
+                </CardTitle>
+                <CardFooter>
+                  <Button disabled className="w-full">
+                    Coming soon!
+                  </Button>
+                </CardFooter>
+              </Card>
+
+              <Card className="p-4 mx-2">
+                <div className="flex justify-center">
+                  <GiUnlitBomb size="70" />
+                </div>
+                <CardTitle className="text-4xl font-extrabold tracking-tight text-center scroll-m-20 text-balance">
+                  Mines
+                </CardTitle>
+                <CardFooter>
+                  <Button
+                    onClick={() => router.push("/play/mines")}
+                    className="w-full"
+                  >
+                    Play now!
+                  </Button>
+                </CardFooter>
+              </Card>
+
+              <Card className="p-4 mx-2">
+                <div className="flex justify-center">
+                  <PiJoystickLight size="70" />
+                </div>
+                <CardTitle className="text-4xl font-extrabold tracking-tight text-center scroll-m-20 text-balance">
+                  Slots
+                </CardTitle>
+                <CardFooter>
+                  <Button disabled className="w-full">
+                    Coming soon!
+                  </Button>
+                </CardFooter>
+              </Card>
+
+              <Card className="p-4 mx-2">
+                <div className="flex justify-center">
+                  <GiPerspectiveDiceSixFacesSix size="70" />
+                </div>
+                <CardTitle className="text-4xl font-extrabold tracking-tight text-center scroll-m-20 text-balance">
+                  And More...
+                </CardTitle>
+                <CardFooter>
+                  <Button disabled className="w-full">
+                    Coming soon!
+                  </Button>
+                </CardFooter>
               </Card>
             </div>
             <h1 className="px-4 mt-10 text-xl font-bold">
@@ -129,16 +198,17 @@ export default function Home() {
             <div className="mt-[5%]">
               <ScrollDown />
             </div>
-            <div className="bottom-2 right-2 absolute">
-              Made by{" "}
-              <Link className="underline" href={"https://internetbowser.com"}>
-                InternetBowser
-              </Link>
-            </div>
+            <Card className="absolute p-2 px-4 bottom-2 right-2">
+              <span>
+                <Link className="underline" href={"https://internetbowser.com"}>
+                  InternetBowser
+                </Link>, 2025
+              </span>
+            </Card>
           </div>
         </div>
 
-        <div className="flex items-center justify-center w-full h-screen bg-background-800">
+        <div className="flex items-center justify-center w-full h-screen bg-zinc-100 dark:bg-zinc-900">
           <h1 className="text-3xl font-bold">
             To start playing, create an account, claim your daily chips and
             navigate to a game.
