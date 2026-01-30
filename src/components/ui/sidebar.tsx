7f55fec5c09d04fcc9ce0af5c18918c0cf2a0cd5
Josh <joshua.ghattas@gmail.com>
2026-01-30 07:26:38 -0600

    Restyle sidebar links

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -10,8 +10,12 @@ import Link from "next/link";
 import ClaimChips from "@/components/ui/global/claim-chips";
 import ChipCount from "@/components/ui/global/chip-count";
 import UserOptions from "./user-options";
+import { useRouter } from "next/navigation";
+import { Separator } from "./separator";
 
 export default function Sidebar() {
+  const router = useRouter();
+
   return (
     <>
       <div className="w-50 h-full! bg-card border-r border-r-foreground/30">
@@ -39,18 +43,36 @@ export default function Sidebar() {
                 <ModeToggle />
               </ButtonGroup>
             </div>
-            <hr className="w-full border" />
+            <Separator />
             <div className="flex flex-col w-full gap-4 mx-2 mt-2 text-left">
               <p className="text-xl font-bold">Games:</p>
-              <Link href="/play/coinflip" className="flex items-center gap-2">
-                <p className="hover:underline">Coinflip</p>
-              </Link>
-              <Link href="/play/blackjack" className="flex items-center gap-2">
-                <p className="hover:underline">Blackjack</p>
-              </Link>
-              <Link href="/play/mines" className="flex items-center gap-2">
-                <p className="hover:underline">Mines</p>
-              </Link>
+              <Button
+                variant={"ghost"}
+                onClick={() => {
+                  router.push("/play/coinflip");
+                }}
+                className="flex items-center justify-start gap-2 text-start"
+              >
+                <p>Coinflip</p>
+              </Button>
+              <Button
+                variant={"ghost"}
+                onClick={() => {
+                  router.push("/play/blackjack");
+                }}
+                className="flex items-center justify-start gap-2 text-start"
+              >
+                <p>Blackjack</p>
+              </Button>
+              <Button
+                variant={"ghost"}
+                onClick={() => {
+                  router.push("/play/mines");
+                }}
+                className="flex items-center justify-start gap-2 text-start"
+              >
+                <p>Mines</p>
+              </Button>
             </div>
           </div>
           <div className="flex flex-col items-center w-full mb-4 overflow-x-hidden">
